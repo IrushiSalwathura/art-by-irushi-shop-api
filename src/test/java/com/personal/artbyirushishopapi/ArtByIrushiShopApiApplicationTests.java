e7c129bcfa986c978570cce7635be26807a4de1b
IrushiSalwathura <irushitharindi@gmail.com>
2025-06-26 10:31:24 +0530

    Initial commit

---
+++ src/test/java/com/personal/artbyirushishopapi/ArtByIrushiShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.artbyirushishopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtByIrushiShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
